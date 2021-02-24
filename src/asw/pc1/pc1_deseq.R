45a1d54c24a3ab78a95ae8795535232b91667bc2
Sarah Inwood <sninwood@gmail.com>
2021-02-25 11:13:26 +1300

    script organising

--- src/asw/pc1/pc1_deseq.R
+++ src/asw/pc1/pc1_deseq.R
@@ -11,10 +11,12 @@ design(asw_dds_pc1) <- ~pc1
 asw_dds_pc1 <- DESeq(asw_dds_pc1)
 
 res_group <- results(asw_dds_pc1, contrast = c("pc1", "positive", "negative"), lfcThreshold = 1, alpha = 0.1)
+summary(res_group)
 ##Order based of padj
 ordered_res_group <- res_group[order(res_group$padj),]
 ##Make data table and write to output
 ordered_res_group_table <- data.table(data.frame(ordered_res_group), keep.rownames = TRUE)
+fwrite(ordered_res_group_table, "output/deseq2/asw/pc1/res_group.csv")
 ordered_sig_res_group_table <- subset(ordered_res_group_table, padj < 0.05)
 sig_annots <- merge(ordered_sig_res_group_table, trinotate, by.x="rn", by.y="#gene_id", all.x=TRUE)
 fwrite(sig_annots, "output/deseq2/asw/pc1/sig_annots.csv")
